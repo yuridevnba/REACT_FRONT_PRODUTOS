3d12001ed06164d700591b81cbb69b5a31c748e1
Yuri12 <yuri.00000830267@unicap.br>
2022-11-11 14:37:24 -0300

    Estrutura do Formulário

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,7 +1,17 @@
  export default function Formulario(){
     return(
-<h1>Formulario</h1>
+<form>
+<input type='text' placeholder='Nome'/>
+<input type='text' placeholder='Marca'/>
+
+<input type='button' value='Cadastrar'/>
+<input type='button' value='Alterar'/>
+<input type='button'value='Remover'/>
+<input type='button' value='Cancelar'/>
 
 
+
+
+</form>
     )
 }
