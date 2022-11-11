076ca9375748ae7bd50204c535dac081c9617c0b
Yuri12 <yuri.00000830267@unicap.br>
2022-11-11 14:44:05 -0300

    Estrutura da Tabela

--- src/Tabela.js
+++ src/Tabela.js
@@ -1,7 +1,26 @@
 export default function Tabela(){
     return(
-<h1>Tabela</h1>
 
+<table>
+<thead>
+<tr>
+<th>#</th>
+<th>Nome</th>
+<th>Marca</th>
+<th>Selecionar</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+        
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+
+    </tr>
+</tbody>
+</table>
 
     )
 }
