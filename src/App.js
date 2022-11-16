22b069ecb4179a71ae3cd89d72e122c3a6355163
Yuri12 <yuri.00000830267@unicap.br>
2022-11-16 11:33:14 -0300

    Alterar Produto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
@@ -8,118 +8,165 @@ function App() {
 
   //Objeto Produto
 
-  const produto={
-  codigo:0,
-   nome:'',
-   marca:''
+  const produto = {
+    codigo: 0,
+    nome: '',
+    marca: ''
 
   }
 
   //UseState
-  const[btnCadastrar,setBtnCadastrar]=useState(true);
-  const[produtos,setProdutos]=useState([]);
-  const[objProdutos,setObjProdutos]=useState(produto);
-  
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProdutos, setObjProdutos] = useState(produto);
+
 
   //UseEffect
-useEffect(()=>{
-  fetch("http://localhost:8080/listar")
-  .then(retorno=>retorno.json())
-  .then(retorno_convertido=>setProdutos(retorno_convertido));
-},[])// [] garanti que ele vai fazer apenas uma vez essa requisição.
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, [])// [] garanti que ele vai fazer apenas uma vez essa requisição.
 
-//Obtendo os dados do formulário
-const aoDigitar=(e)=>{
-  //console.log(e.target);// o objeto que está executando o evento.
-  setObjProdutos({...objProdutos,[e.target.name]:e.target.value})// o valor que está contendo o objeto produto, código, valor e marca.
-}
-   //Cadastrar Produto
-   const Cadastrar = ()=>{
-    fetch('http://localhost:8080/cadastrar',{
-      method:'post',
-      body:JSON.stringify(objProdutos),
-      headers:{
-        'Content-type':'application/json',
-        'Accept':'application/json'
+  //Obtendo os dados do formulário
+  const aoDigitar = (e) => {
+    //console.log(e.target);// o objeto que está executando o evento.
+    setObjProdutos({ ...objProdutos, [e.target.name]: e.target.value })// o valor que está contendo o objeto produto, código, valor e marca.
+  }
+  //Cadastrar Produto
+  const Cadastrar = () => {
+    fetch('http://localhost:8080/cadastrar', {
+      method: 'post',
+      body: JSON.stringify(objProdutos),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
       }
     }) // requisições do tipo get.
-    .then(retorno=>retorno.json()) // promises
-    .then(retorno_convertido=>{
+      .then(retorno => retorno.json()) // promises
+      .then(retorno_convertido => {
 
-      if(retorno_convertido.mensagem!==undefined){
-        // se retorno_convertido, tiver msg, erro
-        alert(retorno_convertido.mensagem)
-      }else{
-        setProdutos([...produtos,retorno_convertido]);
-        alert('Produto cadastrado com sucesso!')
-        limparFormulario()
+        if (retorno_convertido.mensagem !== undefined) {
+          // se retorno_convertido, tiver msg, erro
+          alert(retorno_convertido.mensagem)
+        } else {
+          setProdutos([...produtos, retorno_convertido]);
+          alert('Produto cadastrado com sucesso!')
+          limparFormulario()
+        }
+        //console.log(retorno_convertido);
+      })// quando for convertido para um Json
+  }
+
+
+  //Alterar Produto
+  const alterar = () => {
+    fetch('http://localhost:8080/alterar', {
+      method: 'put',
+      body: JSON.stringify(objProdutos),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
       }
-      //console.log(retorno_convertido);
-    })// quando for convertido para um Json
-   }
+    })
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => {
 
-   //Remover Produto
-   const remover = ()=>{
-    fetch('http://localhost:8080/remover/'+objProdutos.codigo,{
-      method:'delete',
-      
-      headers:{
-        'Content-type':'application/json',
-        'Accept':'application/json'
+        if (retorno_convertido.mensagem !== undefined) {
+
+          alert(retorno_convertido.mensagem)
+        } else {
+          //setProdutos([...produtos,retorno_convertido]);
+
+          //Mensagem
+          alert('Produto cadastrado com sucesso!')
+
+          // Cópia do vetor de produtos
+
+          let vetorTemp = [...produtos]; // acessar ao código,nome e marca.
+
+          // Ìndice
+          let indice = vetorTemp.findIndex((p) => {
+            return p.codigo === objProdutos.codigo;
+            // retorna a posição de alguma verificação.
+            // posição que foi removida do vetor.
+          });
+
+          // Alterar produto do vetorTemp
+
+          vetorTemp[indice] = objProdutos;
+
+          // Atualizar o vetor de produtos
+          setProdutos(vetorTemp);
+           // limpar formulario
+          limparFormulario()
+        }
+
+      })
+  }
+
+  //Remover Produto
+  const remover = () => {
+    fetch('http://localhost:8080/remover/' + objProdutos.codigo, {
+      method: 'delete',
+
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
       }
-    }) 
-    .then(retorno=>retorno.json())
-    .then(retorno_convertido=>{
+    })
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => {
 
-     // Mensagem
-     alert(retorno_convertido.mensagem);
+        // Mensagem
+        alert(retorno_convertido.mensagem);
 
-     // Cópia do vetor de produtos
+        // Cópia do vetor de produtos
 
-     let vetorTemp=[...produtos]; // acessar ao código,nome e marca.
+        let vetorTemp = [...produtos]; // acessar ao código,nome e marca.
 
-     // Ìndice
-     let indice=vetorTemp.findIndex((p)=>{
-       return p.codigo === objProdutos.codigo;
-      // retorna a posição de alguma verificação.
-      // posição que foi removida do vetor.
-    });
+        // Ìndice
+        let indice = vetorTemp.findIndex((p) => {
+          return p.codigo === objProdutos.codigo;
+          // retorna a posição de alguma verificação.
+          // posição que foi removida do vetor.
+        });
 
-    // Remover produto do vetorTemp
+        // Remover produto do vetorTemp
 
-    vetorTemp.splice(indice,1);
-    // Atualizar o vetor de produtos
-    setProdutos(vetorTemp);
+        vetorTemp.splice(indice, 1);
+        // Atualizar o vetor de produtos
+        setProdutos(vetorTemp);
 
-    //Limpar formulário
-    limparFormulario();
-
-
-  })
-}
+        //Limpar formulário
+        limparFormulario();
 
 
-   //Limpar formulário
+      })
+  }
 
-   const limparFormulario=()=>{
+
+  //Limpar formulário
+
+  const limparFormulario = () => {
     setObjProdutos(produto); // modelo sempre vai estar limpo.
     setBtnCadastrar(true);
-   }
+  }
 
-   //Selecionar produto
+  //Selecionar produto
 
-   const selecionarProduto =(indice)=>{
+  const selecionarProduto = (indice) => {
     setObjProdutos(produtos[indice]);
     setBtnCadastrar(false);
 
-   }
+  }
 
   //Retorno
   return (
     <div >
-     
-      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={Cadastrar}  obj={objProdutos} cancelar={limparFormulario}  remover={remover}/>
-      <Tabela  vetor={produtos}  selecionar={selecionarProduto}/>
+
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={Cadastrar} obj={objProdutos} cancelar={limparFormulario} remover={remover} alterar={alterar} />
+      <Tabela vetor={produtos} selecionar={selecionarProduto} />
     </div>
   );
 }
--- src/Formulario.js
+++ src/Formulario.js
@@ -1,4 +1,4 @@
- export default function Formulario({botao,eventoTeclado,cadastrar,obj,cancelar,remover}){
+ export default function Formulario({botao,eventoTeclado,cadastrar,obj,cancelar,remover,alterar}){
     return(
 <form>
 <input type='text' value={obj.nome} onChange={eventoTeclado} name='nome' placeholder='Nome' className='form-control'/>
@@ -10,7 +10,7 @@
     :
     <div>
 
-<input type='button' value='Alterar' className='btn btn-warning'/>
+<input type='button' value='Alterar' onClick={alterar} className='btn btn-warning'/>
 <input type='button'value='Remover' onClick={remover} className='btn btn-danger'/>
 <input type='button' value='Cancelar'  onClick={cancelar} className='btn btn-secondary'/>
 
